0db54159278bbf603c0f7e4b484221e32b7d1683
Or Taizi <ortaizi12@gmail.com>
2025-04-08 18:49:56 -0700

    h

--- frontend/src/pages/dashboard/dashboard.jsx
+++ frontend/src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './dashboard.css';
 
-import Sidebar from './Sidebar';
+import Sidebar from './sidebar';
 import Header from './home/Header';
 import CardsRow from './home/CardsRow';
 import EventsPanel from './home/EventsPanel';
