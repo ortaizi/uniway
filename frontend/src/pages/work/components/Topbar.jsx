ccb062602536b18fc3a3697993f3a65b7ce31e4c
Or Taizi <ortaizi12@gmail.com>
2025-04-09 18:19:51 -0700

    Update Topbar.jsx

--- frontend/src/pages/work/components/Topbar.jsx
+++ frontend/src/pages/work/components/Topbar.jsx
@@ -1,34 +1,13 @@
-// File: Topbar.jsx
 import React from 'react';
+import { FiBell, FiUser } from 'react-icons/fi';
 import './Topbar.css';
-import { FiMessageSquare, FiBell } from 'react-icons/fi';
 
 export default function Topbar() {
   return (
     <header className="topbar">
-      <div className="topbar-left">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/8/82/Jira_%28Software%29_logo.svg"
-          alt="Logo"
-          className="logo"
-        />
-      </div>
-
-      <div className="topbar-center">
-        <input type="text" placeholder="Search..." className="search-input" />
-      </div>
-
-      <div className="topbar-right">
-        <FiMessageSquare className="topbar-icon" />
+      <div className="topbar-icons">
         <FiBell className="topbar-icon" />
-        <div className="profile">
-          <img
-            src="https://randomuser.me/api/portraits/women/44.jpg"
-            alt="User"
-            className="profile-pic"
-          />
-          <span className="profile-name">Courtney Henry</span>
-        </div>
+        <FiUser className="topbar-icon" />
       </div>
     </header>
   );
