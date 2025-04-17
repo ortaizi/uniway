06af61ad5c98452477478e5639ca80b1b5f0d3c2
Or Taizi <ortaizi12@gmail.com>
2025-04-17 14:48:04 -0700

    כגע

--- frontend/src/pages/work/Work.jsx
+++ frontend/src/pages/work/Work.jsx
@@ -8,17 +8,23 @@ function Work() {
   return (
     <div className="work-page">
       <div className="work-container">
+        
+        {/* סיידבר מימין עם רקע בהיר ועיצוב מעוגל */}
         <div className="sidebar-wrapper">
           <Sidebar />
         </div>
 
+        {/* תוכן ראשי */}
         <div className="main-wrapper">
           <Topbar />
+
           <div className="main-content">
+            {/* כל תוכן הדשבורד מופיע כאן */}
             <div className="content-card">
               <Outlet />
             </div>
           </div>
+          
         </div>
       </div>
     </div>
